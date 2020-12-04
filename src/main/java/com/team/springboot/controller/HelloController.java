1141aff00bb57a09a8adfbdb969eeab940140140
吴义琦 <1020303610@qq.com>
2020-12-04 20:37:42 +0800

    增加boot启动类注解

--- src/main/java/com/team/springboot/controller/HelloController.java
+++ src/main/java/com/team/springboot/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.team.springboot.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// RestController注解修饰的类 不支持视图转跳
 @RestController
 public class HelloController {
 
