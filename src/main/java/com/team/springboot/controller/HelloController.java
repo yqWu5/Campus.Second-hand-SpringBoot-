0379a9d2b40cbf23d95079eca18103bfc8916922
吴义琦 <1020303610@qq.com>
2020-12-04 19:44:37 +0800

    SpringBoot初始化

---
+++ src/main/java/com/team/springboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.team.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "hello!SpringBoot!";
+    }
+}
