e72d11c119ce436aee774b4ee5a68fc3ffa99b94
FallWater <8309790+luoshui1@user.noreply.gitee.com>
2020-12-04 20:06:52 +0800

    创建包

--- src/main/java/com/team/springboot/controller/HelloController.java
+++ src/main/java/com/team/springboot/controller/HelloController.java
@@ -10,4 +10,5 @@ public class HelloController {
     public String sayHello(){
         return "hello!SpringBoot!";
     }
+
 }
---
+++ src/main/java/com/team/springboot/pojo/testpojo.java
@@ -0,0 +1,4 @@
+package com.team.springboot.pojo;
+
+public class testpojo {
+}
