190412c342109a3aedda8642d0a53795454391cc
FallWater <8309790+luoshui1@user.noreply.gitee.com>
2020-12-05 16:56:49 +0800

    测试1

--- src/main/java/com/team/springboot/mapper/userMapper.java
+++ src/main/java/com/team/springboot/mapper/userMapper.java
@@ -8,10 +8,15 @@ import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface userMapper {
+
     @Select("select * from user1 where id = #{id}")
     User selectOne(String id);
 
 
-    @Update("update user1 set name = #{name}, email = #{email}, sex = #{sex}, phone = #{phone} where id = #{id}")
+    @Select("select * from user where u_Account = #{id}")
+    User selectOne(int id);
+
+
+    @Update("update user set u_Name = #{name}, u_Email = #{email}, u_Sex = #{sex}, u_Phone = #{phone} where u_Account = #{id}")
     void updateOne(User u);
 }
