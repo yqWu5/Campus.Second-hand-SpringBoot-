9a58426ecd3b489418ec18f644d5796a42a24233
FallWater <8309790+luoshui1@user.noreply.gitee.com>
2020-12-21 15:49:43 +0800

    背景图片11

--- src/main/java/com/team/springboot/mapper/ProductCategoryMapper.java
+++ src/main/java/com/team/springboot/mapper/ProductCategoryMapper.java
@@ -17,13 +17,13 @@ public interface ProductCategoryMapper {
     List<ProductCategory> selectProductCategorys(int page, int limit);
     @Select("select p_Id,p_Account,p_Name,c_Name,p_Title,p_Price,p_href,p_href1 from product inner join Category on product.c_Id=Category.c_Id where p_Account = #{0} limit #{1}, #{2}")
     List<ProductCategory> selectProductCategorysByaccount(String p_Account,int page, int limit);
-    @Select("select p_Id,p_Account,p_Name,c_Name,p_Title,p_Price,p_href from product inner join Category on product.c_Id=Category.c_Id")
+    @Select("select p_Id,p_Account,p_Name,c_Name,p_Title,p_Price,p_href,p_href1 from product inner join Category on product.c_Id=Category.c_Id")
     List<ProductCategory> selectProductAll();
 
-    @Select("select p_Id,p_Account,p_Name,c_Name,p_Title,p_Price from product inner join Category on product.c_Id=Category.c_Id where p_Name like #{2} limit #{0}, #{1}")
+    @Select("select p_Id,p_Account,p_Name,c_Name,p_Title,p_Price,p_href from product inner join Category on product.c_Id=Category.c_Id where p_Name like #{2} limit #{0}, #{1}")
     List<ProductCategory>selectProductCategorysByp_name(int page, int limit,String p_Name);
 
-    @Select("select p_Id,p_Account,p_Name,c_Name,p_Title,p_Price from product inner join Category on product.c_Id=Category.c_Id where p_Name like #{2} AND p_Account = #{3} limit #{0}, #{1}")
+    @Select("select p_Id,p_Account,p_Name,c_Name,p_Title,p_Price,p_href from product inner join Category on product.c_Id=Category.c_Id where p_Name like #{2} AND p_Account = #{3} limit #{0}, #{1}")
     List<ProductCategory>selectProductCategorysByp_nameAndaccount(int page, int limit,String p_Name,String p_Account);
 
     @Select("select c_Name from category")
